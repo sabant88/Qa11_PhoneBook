f045d785342c8e4f17f6e54c9d07caac18dbc926
sabant88 <sabant88@mail.ru>
2022-01-23 20:08:00 +0200

    added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -47,6 +48,7 @@ public class LoginTest {
 
         //check sign out in present
         //  Assert.assertTrue(wd.findElement(By.xpath("//button[text()='Sign Out']")));
+
 
 
 
